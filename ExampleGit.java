4ec05318e8b1fac18b8a6b64aff7cb2d23413263
rodoggx <mikerodolph@yahoo.com>
2016-07-19 17:10:18 -0400

    New Function

--- ExampleGit.java
+++ ExampleGit.java
@@ -3,4 +3,10 @@ public class ExampleGit {
     public static void main(String []args) {
        System.out.println("Hello World");
     }
+	public static int sum (int n1, int n2) {
+       return n1 + n2;
+    }
+	 public static int sum (int n1, int n2) {
+       return n1 * n2;
+    }
 } 
